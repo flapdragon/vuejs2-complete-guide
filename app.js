cfffd638b0782888ca103ce590c820bff4a4e519
David Dean <vermicious.knid.d@gmail.com>
2018-03-01 01:15:16 -0700

    Module 02 lesson 26. :class.

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 new Vue({
 	el: '#app',
   data: {
-		counter: 0,
-		secondCounter: 0,
-		result_icon: ''
+		attachRed: false,
+		attachGreen: false,
+		attachBlue: false,
+		attachTeal: false,
+		progress: 0
   },
-	computed: {
-		output: function() {
-			this.result_icon = this.counter > 5 ? 'keyboard_arrow_right' : 'keyboard_arrow_left'
-			return this.counter > 5 ? '> 5' : '< 5'
-		}
-	},
-	watch: {
-		counter: function(value) {
-			var vm = this
-			setTimeout(function() {
-				vm.counter = 0
-			}, 3000)
-		}
-	},
 	methods: {
-		result: function() {
-			this.result_icon = this.counter > 5 ? 'keyboard_arrow_right' : 'keyboard_arrow_left'
-			return this.counter > 5 ? '> 5' : '< 5'
+		incrementProgress: function() {
+			this.attachTeal = !this.attachTeal
+			if (this.progress < 100) {
+				this.progress += 20
+			}
+			else {
+				this.progress = 0
+			}
 		}
 	}
 })
