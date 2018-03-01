46ab77b5dfb883a849febf83c53215f73b173eb5
David Dean <vermicious.knid.d@gmail.com>
2018-02-28 23:15:17 -0700

    Module 02 lesson 23b.

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ new Vue({
 	el: '#app',
   data: {
 		counter: 0,
+		secondCounter: 0,
 		result_icon: ''
   },
+	computed: {
+		output: function() {
+			console.log('computed')
+			this.result_icon = this.counter > 5 ? 'keyboard_arrow_right' : 'keyboard_arrow_left'
+			return this.counter > 5 ? '> 5' : '< 5'
+		}
+	},
 	methods: {
 		result: function() {
+			console.log('methods')
 			this.result_icon = this.counter > 5 ? 'keyboard_arrow_right' : 'keyboard_arrow_left'
 			return this.counter > 5 ? '> 5' : '< 5'
 		}
