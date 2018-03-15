0eee856c30cd29f9f7308f3e9fe56a80b2c63c2f
David Dean <vermicious.knid.d@gmail.com>
2018-03-15 16:06:21 -0700

    Module 04 lesson 51. Add attack method.

--- app.js
+++ app.js
@@ -8,11 +8,44 @@ new Vue({
   methods: {
     startGame: function() {
       this.gameIsRunning = true
-      this.playHealth = 100
+      this.playerHealth = 100
       this.monsterHealth = 100
     },
     stopGame: function() {
       this.gameIsRunning = false
+    },
+    attack: function() {
+      var heroMax = 10
+      var heroMin = 3
+      var heroDamage = Math.max(Math.floor(Math.random() * heroMax) + 1, heroMin)
+      this.monsterHealth -= heroDamage
+
+      if (this.monsterHealth <= 0) {
+        alert('You won!')
+        this.gameIsRunning = false
+        return
+      }
+
+      var monsterMax = 12
+      var monsterMin = 5
+      var monsterDamage = Math.max(Math.floor(Math.random() * monsterMax) + 1, monsterMin)
+      this.playerHealth -= monsterDamage
+
+      if (this.playerHealth <= 0) {
+        alert('You lost!')
+        this.gameIsRunning = false
+        return
+      }
+
+    },
+    specialAttack: function() {
+
+    },
+    heal: function() {
+
+    },
+    giveUp: function() {
+
     }
   }
 })
