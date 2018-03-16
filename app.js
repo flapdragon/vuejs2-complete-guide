bf3a37fb904d2d19d61ab39f65de8daa49605190
David Dean <vermicious.knid.d@gmail.com>
2018-03-16 13:23:39 -0700

    Module 05 lesson 64. Multiple Vue instances.

--- app.js
+++ app.js
@@ -1,100 +1,33 @@
 new Vue({
-  el: '#app',
+  el: '#app1',
   data: {
-    playerHealth: 100,
-    monsterHealth: 100,
-    gameIsRunning: false,
-    turns: []
+    title: 'The VueJS Instance',
+    showParagraph: false
   },
   methods: {
-    startGame: function() {
-      this.gameIsRunning = true
-      this.playerHealth = 100
-      this.monsterHealth = 100
-      this.turns = []
+    show: function() {
+      this.showParagraph = true;
+      this.updateTitle('The VueJS Instance (Updated)')
     },
-    attack: function() {
-      // damage
-      var damage = this.calculateDamage(3, 10)
-      this.monsterHealth -= damage
-      // add to turns
-      this.turns.unshift({
-        isPlayer: true,
-        text: 'Player hits Monster for ' + damage
-      })
-      if (this.checkWin()) {
-        return
-      }
-
-      this.monsterAttack()
-
-    },
-    specialAttack: function() {
-      // damage
-      var damage = this.calculateDamage(10, 20)
-      this.monsterHealth -= damage
-      // add to turns
-      this.turns.unshift({
-        isPlayer: true,
-        text: 'Player hits Monster hard for ' + damage
-      })
-      if (this.checkWin()) {
-        return
-      }
-
-      this.monsterAttack()
-    },
-    heal: function() {
-      // heal
-      if (this.playerHealth <= 90) {
-        this.playerHealth += 10
-      }
-      else {
-        this.playHealth = 100
-      }
-      // add to turns
-      this.turns.unshift({
-        isPlayer: true,
-        text: 'Player heals for 10'
-      })
-      this.monsterAttack()
-    },
-    giveUp: function() {
-      this.gameIsRunning = false
-    },
-    monsterAttack: function() {
-      var damage = this.calculateDamage(5, 12)
-      this.playerHealth -= damage
-      // add to turns
-      this.turns.unshift({
-        isPlayer: false,
-        text: 'Monster hits Player for ' + damage
-      })
-      this.checkWin()
-    },
-    calculateDamage: function(min, max) {
-      return Math.max(Math.floor(Math.random() * max) + 1, min)
-    },
-    checkWin: function() {
-      if (this.monsterHealth <= 0) {
-        if (confirm('You won! New game?')) {
-          this.startGame()
-        }
-        else {
-          this.stopGame()
-        }
-        return true
-      }
-      else if (this.playerHealth <= 0) {
-        if (confirm('You lost! New game?')) {
-          this.startGame()
-        }
-        else {
-          this.stopGame()
-        }
-        return true
-      }
-      return false
+    updateTitle: function(title) {
+      this.title = title
     }
+  },
+  computed: {
+    lowercaseTitle: function() {
+      return this.title.toLowerCase()
+    }
+  },
+  watch: {
+    title: function(value) {
+      alert('Title changed, new value: ' + value);
+    }
+  }
+})
+
+new Vue({
+  el: '#app2',
+  data: {
+    title: 'The second instance'
   }
 })
