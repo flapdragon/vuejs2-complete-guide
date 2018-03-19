8b57e5be7dfa21ed8014d762b4f79ef529a552e5
David Dean <vermicious.knid.d@gmail.com>
2018-03-19 13:08:59 -0700

    Module 07 lesson 90. Intro. Using a component.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: {
+    status: 'Critical'
+  }
+}
+</script>
+
+<style>
+
+</style>
