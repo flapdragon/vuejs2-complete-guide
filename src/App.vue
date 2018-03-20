a6d750cd940512949d163f7666538f12b83861b2
David Dean <vermicious.knid.d@gmail.com>
2018-03-20 16:49:42 -0700

    Module 09 lesson 118. Setting up Module project.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <app-user></app-user>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote></app-quote>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import User from './components/User.vue';
+import Quote from './components/Quote.vue'
 
-    export default {
-        components: {
-            appUser: User
-        }
-    }
+export default {
+  components: {
+    appQuote: Quote
+  }
+}
 </script>
 
 <style>
-    div.component {
-        border: 1px solid black;
-        padding: 30px;
-    }
 </style>
