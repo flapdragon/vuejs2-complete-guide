ad955b686a98d0f10d3522df830318418a2592a5
David Dean <vermicious.knid.d@gmail.com>
2018-03-19 18:29:24 -0700

    Module 07 lesson 97. Folder structure.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
 </template>
 
 <script>
+import Header from './components/Shared/Header'
+import Footer from './components/Shared/Footer'
+import Servers from './components/Server/Servers'
+import ServerDetails from './components/Server/ServerDetails'
+
 export default {
 }
 </script>
