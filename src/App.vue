cf1ab37501bf1f8d2fa1ba2e5d0577c0d8cd4d50
David Dean <vermicious.knid.d@gmail.com>
2018-03-20 14:15:38 -0700

    Module 08 lesson 105. Using props from parent to child.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div class="container">
-    <app-header></app-header>
-    <hr />
-    <div class="row">
-      <app-servers></app-servers>
-      <app-server-details></app-server-details>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <app-user></app-user>
+            </div>
+        </div>
     </div>
-    <hr />
-    <app-footer></app-footer>
-  </div>
 </template>
 
 <script>
-import Header from './components/Shared/Header'
-import Footer from './components/Shared/Footer'
-import Servers from './components/Server/Servers'
-import ServerDetails from './components/Server/ServerDetails'
+    import User from './components/User.vue';
 
-export default {
-  components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-servers': Servers,
-    'app-server-details': ServerDetails
-  }
-}
+    export default {
+        components: {
+            appUser: User
+        }
+    }
 </script>
 
 <style>
-
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
 </style>
