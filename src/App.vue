7c6cdccfa314ac34aa79cf0da943bbfc3e54e23d
David Dean <vermicious.knid.d@gmail.com>
2018-03-19 18:49:59 -0700

    Module 07 lesson 99. Naming component tags. Also updated .gitignore. Add bootstrap.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <app-servers></app-servers>
+  <div class="container">
+    <app-header></app-header>
+    <hr />
+    <div class="row">
+      <servers></servers>
+      <app-server-details></app-server-details>
+    </div>
+    <hr />
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
@@ -9,6 +18,12 @@ import Servers from './components/Server/Servers'
 import ServerDetails from './components/Server/ServerDetails'
 
 export default {
+  components: {
+    appHeader: Header,
+    'app-footer': Footer,
+    Servers,
+    'app-server-details': ServerDetails
+  }
 }
 </script>
 
