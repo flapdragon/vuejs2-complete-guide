2678128597a3681092ecd12005ecceeef3d598dc
David Dean <vermicious.knid.d@gmail.com>
2018-03-22 16:06:25 -0700

    Module 10 lesson 131. Module introduction.

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,13 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <button @click="selectedComponent = 'appQuote'">Quote</button>
-        <button @click="selectedComponent = 'appAuthor'">Author</button>
-        <button @click="selectedComponent = 'appNew'">New</button>
-        <hr />
-        <p>
-          {{ selectedComponent }}
-        </p>
-        <keep-alive>
-          <component :is="selectedComponent"></component>
-        </keep-alive>
-        <!-- <app-quote>
-          <h2 slot="title">{{ quoteTitle }}</h2>
-          <p>A wonderful quote</p>
-        </app-quote> -->
-      </div>
+    <div class="container">
+    
     </div>
-  </div>
 </template>
 
 <script>
-import Quote from './components/Quote.vue'
-import Author from './components/Author.vue'
-import New from './components/New.vue'
-
-export default {
-  data: function() {
-    return {
-      quoteTitle: 'The Quote',
-      selectedComponent: 'app-quote'
+    export default {
+        
     }
-  },
-  components: {
-    appQuote: Quote,
-    appAuthor: Author,
-    appNew: New
-  }
-}
 </script>
 
 <style>
