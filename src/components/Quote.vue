30d052e52eb22fb507343ead9b1d98c64490ea72
David Dean <vermicious.knid.d@gmail.com>
2018-03-22 17:28:09 -0700

    Module 10 lesson 134. Creating the application components.

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-
+  <div class="col-sm-6 col=md4 col-lg-3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
+    props: [ 'quote' ]
   }
 </script>
 
 <style>
+.panel-body {
+  font-family: 'Arizonia', cursive;
+  font-size: 24px;
+  color: #6E6E6E;
+}
+
+.quote {
+  cursor: pointer;
+}
+
+.quote:hover {
+  background-color: #FFE2E2;
+}
 </style>
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-
+  <div class="row">
+    <app-quote v-for="quote in quotes">{{ quote }}</app-quote>
   </div>
 </template>
 
 <script>
+  import Quote from './Quote.vue'
+
   export default {
+    props: [ 'quotes' ],
+    components: {
+      appQuote: Quote
+    }
   }
 </script>
 
