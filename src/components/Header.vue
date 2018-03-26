b3cc92dc1954139c6a5d9e2c83e5f1895a62873c
David Dean <vermicious.knid.d@gmail.com>
2018-03-26 15:11:27 -0700

    Module 16 lesson 229. Add route parameter to user route.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="nav nav-pills">
   <router-link to="/" tag="li" class="nav-link" active-class="active" exact><a>Home</a></router-link>
-  <router-link to="/user" tag="li" class="nav-link" active-class="active"><a>User</a></router-link>
+  <router-link to="/user/10" tag="li" class="nav-link" active-class="active"><a>User</a></router-link>
 </ul>
 </template>
