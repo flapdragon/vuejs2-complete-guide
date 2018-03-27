9f3b7414294bc9f6b71c8a05ef335950a7b8fd72
David Dean <vermicious.knid.d@gmail.com>
2018-03-27 16:16:37 -0700

    Module 16 lesson 235. Add user edit button and router-link.

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -4,5 +4,6 @@
     <p>
       User loaded has ID: {{ $route.params.id }}
     </p>
+    <router-link tag="button" :to="'/user/' + $route.params.id + '/edit'" class="btn btn-primary">Edit User</router-link>
   </div>
 </template>
