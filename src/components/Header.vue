41bb15be41c5d390c0589af9a783c7dd7b252cee
David Dean <vermicious.knid.d@gmail.com>
2018-03-27 16:07:52 -0700

    Module 16 lesson 233. Add child routes to User. Remove hard coded routes.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,6 @@
 <template>
   <ul class="nav nav-pills">
   <router-link to="/" tag="li" class="nav-link" active-class="active" exact><a>Home</a></router-link>
-  <router-link to="/user/1" tag="li" class="nav-link" active-class="active"><a>User 1</a></router-link>
-  <router-link to="/user/2" tag="li" class="nav-link" active-class="active"><a>User 2</a></router-link>
+  <router-link to="/user" tag="li" class="nav-link" active-class="active"><a>User</a></router-link>
 </ul>
 </template>
