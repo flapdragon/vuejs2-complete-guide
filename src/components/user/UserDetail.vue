a8161a99d6510df4a028f35be1777538fcd73208
David Dean <vermicious.knid.d@gmail.com>
2018-03-29 13:33:53 -0700

    Module 16 lesson 236. Replace hardcoded links with named routes.

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -4,6 +4,6 @@
     <p>
       User loaded has ID: {{ $route.params.id }}
     </p>
-    <router-link tag="button" :to="'/user/' + $route.params.id + '/edit'" class="btn btn-primary">Edit User</router-link>
+    <router-link tag="button" :to="{ name: 'userEdit', params: { id: $route.params.id } }" class="btn btn-primary">Edit User</router-link>
   </div>
 </template>
