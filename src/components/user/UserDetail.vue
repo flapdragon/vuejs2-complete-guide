27c3bb2ab72f1ec5d6530ce754540cd466e34972
David Dean <vermicious.knid.d@gmail.com>
2018-03-29 13:38:49 -0700

    Module 16 lesson 237. Add query parameters to route object.

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -4,6 +4,6 @@
     <p>
       User loaded has ID: {{ $route.params.id }}
     </p>
-    <router-link tag="button" :to="{ name: 'userEdit', params: { id: $route.params.id } }" class="btn btn-primary">Edit User</router-link>
+    <router-link tag="button" :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }" class="btn btn-primary">Edit User</router-link>
   </div>
 </template>
