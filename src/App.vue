f501581757280ba2c182b77486919c63aab86401
David Dean <vermicious.knid.d@gmail.com>
2018-04-10 18:44:36 -0700

    Module 13 lesson 169. Initial module setup.

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,17 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Directives</h1>
-        <p v-text="'Some text'"></p>
-        <p v-html="'<strong>Einige starker Text</strong>'"></p>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+
+            </div>
+        </div>
     </div>
-    <hr />
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Custom Directives</h1>
-        <p v-highlight:background.delayed="'red'">Color this</p>
-        <p v-local-highlight:background.delayed.blink="{mainColor: 'bisque', secondColor: 'darksalmon', delay: 500}">Color this, auch</p>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
-  export default {
-    directives: {
-      'local-highlight': {
-        bind (el, binding, vnode) {
-          let delay = 0
-          if (binding.modifiers['delayed']) {
-            delay = 3000
-          }
-          if (binding.modifiers['blink']) {
-            const mainColor = binding.value.mainColor
-            const secondColor = binding.value.secondColor
-            let currentColor = mainColor
-            setTimeout(() => {
-              setInterval(() => {
-                currentColor === secondColor ? currentColor = mainColor : currentColor = secondColor
-                if (binding.arg === 'background') {
-                  el.style.backgroundColor = currentColor
-                } else {
-                  el.style.color = currentColor
-                }
-              }, binding.value.delay)
-            }, delay)
-          }
-          else {
-            setTimeout(() => {
-              if (binding.arg === 'background') {
-                el.style.backgroundColor = binding.value.mainColor
-              } else {
-                el.style.color = binding.value.mainColor
-              }
-            }, delay)
-          }
-        }
-      }
+    export default {
     }
-  }
 </script>
 
 <style>
