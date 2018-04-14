1ef9a9b0e426156c90578e54c23a2453e8fd5540
David Dean <vermicious.knid.d@gmail.com>
2018-04-14 13:54:27 -0700

    Module 13 lesson 174. Add mixin for fruit data.

--- src/App.vue
+++ src/App.vue
@@ -11,33 +11,33 @@
         <ul>
            <li v-for="fruit in filteredFruits">{{ fruit }}</li>
         </ul>
+        <hr />
+        <app-list></app-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-      data () {
-        return {
-          text: 'Hello there!',
-          fruits: [ 'Apple', 'Banana', 'Mango', 'Melon', 'Durian' ],
-          filterText: ''
-        }
-      },
-      filters: {
-        'toUppercase' (value) {
-          return value.toUpperCase()
-        }
-      },
-      computed: {
-        filteredFruits() {
-          return this.fruits.filter((element) => {
-            return element.match(this.filterText)
-          })
-        }
+  import List from './List.vue'
+  import { fruitMixin } from './fruitMixin'
+
+  export default {
+    mixins: [ fruitMixin ],
+    data () {
+      return {
+        text: 'Hello there!'
       }
+    },
+    filters: {
+      'toUppercase' (value) {
+        return value.toUpperCase()
+      }
+    },
+    components: {
+      appList: List
     }
+  }
 </script>
 
 <style>
