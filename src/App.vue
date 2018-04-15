ea1ca06426a349e3de4303a0dab0dc496f965337
David Dean <vermicious.knid.d@gmail.com>
2018-04-15 12:37:56 -0700

    Module 14 lesson 180. Initial module setup.

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,22 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Filters & Mixins</h1>
-        <p>
-          {{ text | toUppercase | to-lowercase }}
-        </p>
-        <hr />
-        <button @click="fruits.push('Raspberry')">Add New Fruit</button>
-        <input v-model="filterText" />
-        <ul>
-           <li v-for="fruit in filteredFruits">{{ fruit }}</li>
-        </ul>
-        <hr />
-        <app-list></app-list>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-  import List from './List.vue'
-  import { fruitMixin } from './fruitMixin'
+    export default {
+        data() {
+            return {
 
-  export default {
-    mixins: [ fruitMixin ],
-    data () {
-      return {
-        text: 'Hello there!'
-      }
-    },
-    filters: {
-      'toUppercase' (value) {
-        return value.toUpperCase()
-      }
-    },
-    components: {
-      appList: List
+            }
+        }
     }
-  }
 </script>
 
 <style>
