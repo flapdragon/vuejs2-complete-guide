2328ea3ac54683f64b4d47137d60c3395bda73b1
David Dean <vermicious.knid.d@gmail.com>
2018-04-16 20:34:45 -0700

    Module 14 lesson 187. Mix transition and animation CSS. Add type to transition HTML.

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
         <transition name="fade">
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
-        <transition name="slide">
+        <transition name="slide" type="animation">
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
       </div>
@@ -43,16 +43,20 @@
   }
 
   .slide-enter {
+    opacity: 0;
     /* transform: translateY(20px); */
   }
   .slide-enter-active {
     animation: slide-in 1s ease-out forwards;
+    transition: opacity 1s;
   }
   .slide-leave {
 
   }
   .slide-leave-active {
    animation: slide-out 1s ease-out forwards;
+   transition: opacity 3s;
+   opacity: 0;
   }
   @keyframes slide-in {
     from {
