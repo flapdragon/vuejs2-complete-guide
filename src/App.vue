c45ed5143449860693089bfd36f9a9f0fff77f73
David Dean <vermicious.knid.d@gmail.com>
2018-04-17 20:11:47 -0700

    Add animation to add/remove items in list.

--- src/App.vue
+++ src/App.vue
@@ -52,8 +52,11 @@
         </transition>
         <hr />
         <button class="btn btn-info" @click="addItem">Add Item</button>
+        <br /><br />
         <ul class="list-group">
-          <li class="list-group-item" v-for="(number, index) in numbers" @click="removeItem(index)" style="cursor: pointer;">{{ number }}</li>
+          <transition-group name="slide">
+            <li class="list-group-item" v-for="(number, index) in numbers" @click="removeItem(index)" style="cursor: pointer;" :key="number">{{ number }}</li>
+          </transition-group>
         </ul>
       </div>
     </div>
@@ -167,6 +170,10 @@
    animation: slide-out 1s ease-out forwards;
    transition: opacity 1s;
    opacity: 0;
+   position: absolute;
+  }
+  .slide-move {
+    transition: transform 1s;
   }
   @keyframes slide-in {
     from {
