8af55b20e06204c6d9a4997e075f3b41d5b386ac
David Dean <vermicious.knid.d@gmail.com>
2018-04-21 18:02:40 -0700

    Module 15 lesson 214. Add vue-resource http interceptor. A more thorough discussion of REST and POST/PUT/etc methods might be in order.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import App from './App.vue'
 Vue.use(VueResource)
 
 Vue.http.options.root = 'http://localhost:3000/firebase'
+Vue.http.interceptors.push((request, next) => {
+  console.log(request)
+  if (request.method === 'POST') {
+    request.method = 'PUT' // Great to know. Don't think you should ever do this.
+  }
+  next()
+})
 
 new Vue({
   el: '#app',
