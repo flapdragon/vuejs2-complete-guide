02298d8ea6f00606e3c994b47f2266bf61f8f58d
David Dean <vermicious.knid.d@gmail.com>
2018-04-22 01:33:01 -0700

    Module 17 lesson 250. Initial module setup.

--- src/App.vue
+++ src/App.vue
@@ -1,132 +1,30 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Http</h1>
-        <div class="form-group">
-          <label>Username</label>
-          <input class="form-control" type="text" v-model="user.username" />
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Vuex</h1>
+                <app-result :counter="counter"></app-result>
+                <hr>
+                <app-counter @updated="counter += $event"></app-counter>
+            </div>
         </div>
-        <div class="form-group">
-          <label>Mail</label>
-          <input class="form-control" type="text" v-model="user.email" />
-        </div>
-        <button class="btn btn-primary" @click="submit">Submit</button>
-        <hr />
-        <button class="btn btn-primary" @click="fetchDataLocal">Get Local Data</button>
-        <ul class="list-group">
-          <li class="list-group-item" v-for="u in usersLocal">
-            {{ u.username }} - {{ u.email }}
-          </li>
-        </ul>
-        <br /><br />
-        <button class="btn btn-primary" @click="fetchDataFirebase">Get Firebase Data</button>
-        <ul class="list-group">
-          <li class="list-group-item" v-for="u in usersFirebase">
-            {{ u.username }} - {{ u.email }}
-          </li>
-        </ul>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-  import * as firebase from "firebase"
+    import Counter from './components/Counter.vue';
+    import Result from './components/Result.vue';
 
-  const config = {
-    apiKey: "AIzaSyC2Aq_OUntc9cyT-v7Hma5IRwEdpMRFj0Q",
-    authDomain: "vuejs-http-e0ac7.firebaseapp.com",
-    databaseURL: "https://vuejs-http-e0ac7.firebaseio.com",
-    storageBucket: "vuejs-http-e0ac7.appspot.com"
-  }
-  firebase.initializeApp(config)
-
-  export default {
-    data() {
-      return {
-        user: {
-          username: '',
-          email: ''
+    export default {
+        data() {
+            return {
+                counter: 0
+            }
         },
-        usersLocal: [],
-        usersFirebase: [],
-        resource: {},
-        node: 'firebase'
-      }
-    },
-    methods: {
-      submit () {
-        if (this.user.username) {
-          // this.$http.post('firebase', this.user)
-          //   .then(response => {
-          //     console.log(response)
-          //   }, error => {
-          //     console.log(error)
-          //   })
-          // this.resource.save({}, this.user)
-          this.resource.saveAlt(this.user)
-
-          // New user
-          const newUserKey = firebase.database().ref().child('users').push().key
-          let updates = {}
-          updates['/users/' + newUserKey] = this.user
-          firebase.database().ref().update(updates)
-
-          // Update user
-          // firebase.database().ref('users/' + newUserKey).set({
-          //   username: this.user.username,
-          //   email: this.user.email
-          // })
+        components: {
+            appCounter: Counter,
+            appResult: Result,
         }
-      },
-      fetchDataLocal () {
-        // vue-resource
-        // this.$http.get('firebase', this.user)
-        //   .then(response => {
-        //     console.log(response)
-        //     return response.json()
-        //   })
-        //   .then(data => {
-        //     console.log(data)
-        //     this.usersLocal = data
-        //   }, error => {
-        //     console.log(error)
-        //   })
-        this.resource.getData({ node: this.node })
-          .then(response => {
-            return response.json()
-          })
-          .then(data => {
-            const messages = data.messages
-            const result = []
-            for (let key in messages) {
-              result.push(messages[key])
-            }
-            this.usersLocal = result
-          })
-      },
-      fetchDataFirebase () {
-        return firebase.database().ref('/users').once('value')
-          .then(snapshot => {
-            const result = []
-            const data = snapshot.val()
-            for (let key in data) {
-              result.push(data[key])
-            }
-            this.usersFirebase = result
-          })
-      }
-    },
-    created () {
-      const customActions = {
-        saveAlt: { method: 'POST', url: 'alternative' },
-        getData: { method: 'GET' }
-      }
-      this.resource = this.$resource('{node}', {}, customActions)
     }
-  }
 </script>
 
-<style>
-</style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,5 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import App from './App.vue'
-
-Vue.use(VueResource)
-
-Vue.http.options.root = 'http://localhost:3000'
-Vue.http.interceptors.push((request, next) => {
-  // if (request.method === 'POST') {
-  //   request.method = 'PUT'
-  // }
-  next(response => {
-    response.json = () => {
-      return { messages: response.body }
-    }
-  })
-})
 
 new Vue({
   el: '#app',
