faae984ea53be92c999356d3a314b81db2d368be
David Dean <vermicious.knid.d@gmail.com>
2018-04-24 23:13:22 -0700

    Module 17 lesson 263. Add actions to handle async changes to mutations. Map actions to methods.

--- src/store/store.js
+++ src/store/store.js
@@ -16,11 +16,29 @@ export const store = new Vuex.Store({
     }
   },
   mutations: {
-    increment: state => {
-      state.counter++
+    increment: (state, payload = 1) => {
+      state.counter += payload
     },
-    decrement: state => {
-      state.counter--
+    decrement: (state, payload = 1) => {
+      state.counter -= payload
+    }
+  },
+  actions: {
+    increment: (context, payload) => { // just wanted one example with context, instead of desctructured commit part of context
+      context.commit('increment', payload)
+    },
+    decrement: ({commit}, payload) => {
+      commit('decrement', payload)
+    },
+    asyncIncrement: ({commit}, payload) => {
+      setTimeout(() => {
+        commit('increment', payload.by)
+      }, payload.duration)
+    },
+    asyncDecrement: ({commit}, payload) => {
+      setTimeout(() => {
+        commit('decrement', payload.by)
+      }, payload.duration)
     }
   }
 })
