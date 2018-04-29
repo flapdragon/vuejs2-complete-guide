58bf8d75019ccf09b35cd0bb886ee9c23345ef8e
David Dean <vermicious.knid.d@gmail.com>
2018-04-29 16:09:32 -0700

    Module 18 lesson 281. Add Vuex. Convert manual data to store.

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -13,20 +13,13 @@
   import Stock from './Stock.vue'
 
   export default {
-    data () {
-      return {
-        stocks: [
-          { id: 1, name: 'BMW', price: 91.50 },
-          { id: 2, name: 'Google', price: 1031.45 },
-          { id: 3, name: 'Apple', price: 162.32 },
-          { id: 4, name: 'Twitter', price: 29.00 },
-          { id: 5, name: 'Amazon', price: 1572.62 },
-          { id: 6, name: 'Microsoft', price: 95.82 }
-        ]
-      }
-    },
     components: {
       appStock: Stock
+    },
+    computed: {
+      stocks () {
+        return this.$store.getters.stocks
+      }
     }
   }
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,8 @@
+export default [
+  { id: 1, name: 'BMW', price: 91.50 },
+  { id: 2, name: 'Google', price: 1031.45 },
+  { id: 3, name: 'Apple', price: 162.32 },
+  { id: 4, name: 'Twitter', price: 29.00 },
+  { id: 5, name: 'Amazon', price: 1572.62 },
+  { id: 6, name: 'Microsoft', price: 95.82 }
+]
